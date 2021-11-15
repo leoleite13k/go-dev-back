00f943199993957d2cb3196b2cef367f15dff954
Leo Leite <leonardotleite13k@gmail.com>
2021-11-15 17:56:21 -0300

    feat: added points and levels

--- app/Models/Profile.ts
+++ app/Models/Profile.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, computed } from '@ioc:Adonis/Lucid/Orm'
+
+const POINTS_PER_LEVEL = 500
 
 export default class Profile extends BaseModel {
   @column({ isPrimary: true, serializeAs: null })
@@ -22,4 +24,14 @@ export default class Profile extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
   public updatedAt: DateTime
+
+  @computed()
+  public get percent_level() {
+    return this.points / POINTS_PER_LEVEL - Math.floor(this.points / POINTS_PER_LEVEL)
+  }
+
+  @computed()
+  public get level() {
+    return Math.floor(this.points / POINTS_PER_LEVEL)
+  }
 }
