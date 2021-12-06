0a07e103b175758930752108fca493d00a6fca72
Leo Leite <leonardotleite13k@gmail.com>
2021-12-06 17:47:06 -0300

    refactor: check if has achievement

--- app/Controllers/Http/UserTrackLessonsController.ts
+++ app/Controllers/Http/UserTrackLessonsController.ts
@@ -2,6 +2,7 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
 import Lesson from 'App/Models/Lesson'
 import Profile from 'App/Models/Profile'
+import Achivement from 'App/Models/Achivement'
 import UserAchivement from 'App/Models/UserAchivement'
 import UserTrackLesson from 'App/Models/UserTrackLesson'
 
@@ -36,49 +37,67 @@ export default class UserTrackLessonsController {
         })
         .save()
 
-      await UserAchivement.firstOrCreate({
-        achivementId: 1,
-        userId: auth.user?.id,
-      })
+      const achivement1 = await Achivement.findBy('id', 1)
 
-      const completedTrack = await Database.from('lessons')
-        .count('lessons.id', 'numberOfLessons')
-        .count('user_track_lessons.id', 'numberOfLessonsCompleted')
-        .leftJoin('user_track_lessons', 'lessons.id', '=', 'user_track_lessons.lesson_id')
-        .where('lessons.track_id', trackId)
-        .first()
-
-      if (completedTrack.numberOfLessons === completedTrack.numberOfLessonsCompleted) {
+      if (achivement1) {
         await UserAchivement.firstOrCreate({
-          achivementId: 2,
+          achivementId: 1,
           userId: auth.user?.id,
         })
       }
 
-      const focusTrack = await Database.from('user_track_lessons')
-        .count('user_track_lessons.id', 'numberOfLessonsCompleted')
-        .first()
+      const achivement2 = await Achivement.findBy('id', 2)
 
-      if (focusTrack.numberOfLessonsCompleted > 10) {
-        await UserAchivement.firstOrCreate({
-          achivementId: 3,
-          userId: auth.user?.id,
-        })
-      }
+      if (achivement2) {
+        const completedTrack = await Database.from('lessons')
+          .count('lessons.id', 'numberOfLessons')
+          .count('user_track_lessons.id', 'numberOfLessonsCompleted')
+          .leftJoin('user_track_lessons', 'lessons.id', '=', 'user_track_lessons.lesson_id')
+          .where('lessons.track_id', trackId)
+          .first()
 
-      const rankingTop5 = await Database.from('profiles').limit(5).orderBy('points', 'desc')
-
-      if (rankingTop5.find((profile) => profile.user_id === auth.user?.id)) {
-        await UserAchivement.firstOrCreate({
-          achivementId: 4,
-          userId: auth.user?.id,
-        })
-
-        if (rankingTop5[0].user_id === auth.user?.id) {
+        if (completedTrack.numberOfLessons === completedTrack.numberOfLessonsCompleted) {
           await UserAchivement.firstOrCreate({
-            achivementId: 5,
+            achivementId: 2,
             userId: auth.user?.id,
           })
+        }
+      }
+
+      const achivement3 = await Achivement.findBy('id', 3)
+
+      if (achivement3) {
+        const focusTrack = await Database.from('user_track_lessons')
+          .count('user_track_lessons.id', 'numberOfLessonsCompleted')
+          .first()
+
+        if (focusTrack.numberOfLessonsCompleted > 10) {
+          await UserAchivement.firstOrCreate({
+            achivementId: 3,
+            userId: auth.user?.id,
+          })
+        }
+      }
+
+      const achivement4 = await Achivement.findBy('id', 4)
+
+      if (achivement4) {
+        const rankingTop5 = await Database.from('profiles').limit(5).orderBy('points', 'desc')
+
+        if (rankingTop5.find((profile) => profile.user_id === auth.user?.id)) {
+          await UserAchivement.firstOrCreate({
+            achivementId: 4,
+            userId: auth.user?.id,
+          })
+
+          const achivement5 = await Achivement.findBy('id', 5)
+
+          if (achivement5 && rankingTop5[0].user_id === auth.user?.id) {
+            await UserAchivement.firstOrCreate({
+              achivementId: 5,
+              userId: auth.user?.id,
+            })
+          }
         }
       }
 
